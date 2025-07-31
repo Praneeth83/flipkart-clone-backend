a3e7ead6df9ab1d221ab54fc1f99f7b4dc678c85
Praneeth83 <167945790+Praneeth83@users.noreply.github.com>
2025-07-31 10:37:40 +0530

    gg

--- config/db.go
+++ config/db.go
@@ -4,18 +4,32 @@ import (
 	"fmt"
 	"log"
 	"test1/models"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
 	dsn := "postgresql://flipkart_clone_user:DdAKsuDfNeN52QCeudB76EJeL40kbsM0@dpg-d25f723e5dus73a1bh30-a.oregon-postgres.render.com/flipkart_clone"
-	var err error
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	gormConfig := &gorm.Config{
+		Logger: logger.New(
+			log.New(log.Writer(), "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Warn,
+				IgnoreRecordNotFoundError: true,
+				Colorful:                  true,
+			},
+		),
+	}
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), gormConfig)
 	if err != nil {
 		log.Fatal("❌ Failed to connect to DB:", err)
 	}
@@ -26,4 +40,11 @@ func ConnectDB() {
 	if err != nil {
 		log.Fatal("❌ Auto migration failed:", err)
 	}
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatal("❌ Failed to get sql.DB from GORM:", err)
+	}
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 }
